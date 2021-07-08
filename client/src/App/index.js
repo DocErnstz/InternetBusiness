c45405e54237800a1c2ad8371e694a867ecfea4b
DocErnstz <ernesto800@outlook.com>
2021-07-08 12:30:42 -0300

    menu navbar responsive

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,3 +1,17 @@
 
 require("./css/index.css");
 
+
+
+
+const hamburger = document.getElementById("hamburguer");
+const navLinks = document.querySelector("ul");
+const links = document.querySelectorAll("li");
+
+hamburger.addEventListener("click", () => {
+  console.log("a");
+  navLinks.classList.toggle("open");
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
+});
