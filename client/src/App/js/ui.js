bc94d5e7a5e572899374ca41b2c5f0fdab4a8f8e
DocErnstz <ernesto800@outlook.com>
2021-08-19 17:39:10 -0300

    swaped out http for https

--- client/src/App/js/ui.js
+++ client/src/App/js/ui.js
@@ -1,14 +1,13 @@
 export class UI {
-    // Add a new Product
-    getMessages(){
-        fetch("http://makoreact.herokuapp.com/messages").then(
-          async (response) => {
-            const data = await response.json();
-            const board = document.getElementById("Board");
+  // Add a new Product
+  getMessages() {
+    fetch("https://makoreact.herokuapp.com/messages").then(async (response) => {
+      const data = await response.json();
+      const board = document.getElementById("Board");
 
-            data.forEach((item) => {
-              const element = document.createElement("div");
-              element.innerHTML = `
+      data.forEach((item) => {
+        const element = document.createElement("div");
+        element.innerHTML = `
     <div class="msg">
     <div class="profile_icon"><i class="fa fa-user"></i></div>
     <div class="profile_data">
@@ -17,30 +16,26 @@ export class UI {
     </div>
     </div>
         `;
-              board.appendChild(element);
-            });
-          }
-        );
-    }
-    addMessages(msg){
-        fetch("http://makoreact.herokuapp.com/messages/message", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            name: msg.name,
-            email: msg.email,
-            message: msg.message,
-          }),
-        })
-          .then(async (response) => {
-            const message = await response.json();
-            console.log(message);
-          })
-          .catch((error) => console.log(error));
-    }
-    
+        board.appendChild(element);
+      });
+    });
   }
-
- 
+  addMessages(msg) {
+    fetch("https://makoreact.herokuapp.com/messages/message", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: msg.name,
+        email: msg.email,
+        message: msg.message,
+      }),
+    })
+      .then(async (response) => {
+        const message = await response.json();
+        console.log(message);
+      })
+      .catch((error) => console.log(error));
+  }
+}
